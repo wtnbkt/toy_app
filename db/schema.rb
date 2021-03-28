3e5f21409a5abe80645147861909ae88f0608941
wtnbkt <ktwbis19@gmail.com>
2021-03-28 23:42:26 +0900

    途中です

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_21_195223) do
+ActiveRecord::Schema.define(version: 2021_03_28_141627) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_12_21_195223) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password"
   end
 
 end
